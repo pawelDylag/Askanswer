3c146325a13c695834e8ef5de49981358dd8fe45
Paweł Dyląg <p.dyloo@gmail.com>
2014-10-25 02:38:22 +0200

    RespondentActivity

--- app/src/main/java/com/aghacks/askanswer/activities/IntroActivity.java
+++ app/src/main/java/com/aghacks/askanswer/activities/IntroActivity.java
@@ -1,19 +1,28 @@
-package com.aghacks.askanswer;
+package com.aghacks.askanswer.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.aghacks.askanswer.R;
 
 
 public class IntroActivity extends Activity {
 
+    private final String TAG = this.getLocalClassName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-    }
+        setButtons();
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +41,25 @@ public class IntroActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setButtons() {
+        Button button_q = (Button) findViewById(R.id.button_q);
+        Button button_r = (Button) findViewById(R.id.button_r);
+
+        button_q.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "button_q onClick ");
+            }
+        });
+
+        button_r.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "button_r onClick ");
+            }
+        });
+
     }
 }
